8097ebbd0706eb888ca828dab2c49211ff2df2f3
Aatif Syed <aatifsyedyp@gmail.com>
2023-09-22 11:39:51 +0100

    feat: simpler types in dsl

--- src/dsl.rs
+++ src/dsl.rs
@@ -1,9 +1,9 @@
 use derive_syn_parse::Parse;
 use proc_macro2::Ident;
 use syn::{
+    braced,
     parse::{Parse, ParseStream},
-    punctuated::Punctuated,
-    token, Attribute, LitStr, Token, Type,
+    token, Attribute, LitStr, Token, Type, Visibility,
 };
 
 pub mod pun {
@@ -14,31 +14,54 @@ pub mod pun {
 #[test]
 fn parse_dsl() {
     let dsl: Dsl = syn::parse_quote! {
-        /// a node description
-        Foo { bar: String };
-        /// an edge
-        Foo --> Bar;
-        /// many edges
-        Foo --> Bar -> Baz;
-        /// another edge
-        Foo -"with inline docs"-> Bar;
-        Foo -"and"-> Bar -"a"-> Few;
+        /// These are state machine docs
+        /// There will be a nice diagram here too
+        pub TrafficLight {
+            /// a node description
+            Foo;
+            /// This node has data associated with it
+            Bar: String;
+
+            /// an edge
+            Foo -> Bar;
+
+            /// many edges
+            Foo --> Bar -> Baz;
+
+            Foo -"with inline docs"-> Bar;
+
+            /// This documentation is shared among the edges
+            And -"some"-> Inline -"documentation"-> Too;
+        }
     };
     dbg!(dsl);
 }
 
 #[cfg_attr(test, derive(Debug, PartialEq, Eq))]
 pub struct Dsl {
+    pub attrs: Vec<Attribute>,
+    pub vis: Visibility,
+    pub name: Ident,
+    pub brace_token: token::Brace,
     pub stmts: Vec<Stmt>,
 }
 
 impl Parse for Dsl {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        let mut stmts = vec![];
-        while !input.is_empty() {
-            stmts.push(input.parse()?)
-        }
-        Ok(Self { stmts })
+        let content;
+        Ok(Self {
+            attrs: input.call(Attribute::parse_outer)?,
+            vis: input.parse()?,
+            name: input.parse()?,
+            brace_token: braced!(content in input),
+            stmts: {
+                let mut stmts = vec![];
+                while !content.is_empty() {
+                    stmts.push(content.parse()?)
+                }
+                stmts
+            },
+        })
     }
 }
 
@@ -64,16 +87,16 @@ pub struct StmtEdges {
     /// Attributes will be stolen from the first Node
     #[call(Attribute::parse_outer)]
     pub attrs: Vec<Attribute>,
-    pub from: Node,
+    pub from: Ident,
     pub edge: Edge,
-    pub to: Node,
+    pub to: Ident,
     #[call(Self::parse_rest)]
-    pub rest: Vec<(Edge, Node)>,
+    pub rest: Vec<(Edge, Ident)>,
     pub semi: Token![;],
 }
 
 impl StmtEdges {
-    fn parse_rest(input: ParseStream) -> syn::Result<Vec<(Edge, Node)>> {
+    fn parse_rest(input: ParseStream) -> syn::Result<Vec<(Edge, Ident)>> {
         let mut rest = vec![];
         while !input.peek(Token![;]) {
             rest.push((input.parse()?, input.parse()?))
@@ -85,68 +108,13 @@ impl StmtEdges {
 #[derive(Parse)]
 #[cfg_attr(test, derive(Debug, PartialEq, Eq))]
 pub struct StmtNode {
-    pub node: Node,
-    pub semi: Token![;],
-}
-
-#[derive(Parse)]
-#[cfg_attr(test, derive(Debug, PartialEq, Eq))]
-pub struct Node {
     #[call(Attribute::parse_outer)]
     pub attrs: Vec<Attribute>,
     pub ident: Ident,
-    #[peek_with(has_brace_or_paren_or_eq)]
-    pub fields: Option<NodeFields>,
-}
-
-fn has_brace_or_paren_or_eq(input: ParseStream) -> bool {
-    input.peek(token::Brace) || input.peek(token::Paren) || input.peek(Token![=])
-}
-
-#[derive(Parse)]
-#[cfg_attr(test, derive(Debug, PartialEq, Eq))]
-pub enum NodeFields {
-    #[peek(Token![=], name = "=")]
-    Alias(Alias),
-    #[peek(token::Paren, name = "(")]
-    TupleLike(TupleLike),
-    #[peek(token::Brace, name = "{")]
-    RecordLike(RecordLike),
-}
-
-#[derive(Parse)]
-#[cfg_attr(test, derive(Debug, PartialEq, Eq))]
-pub struct Alias {
-    pub eq: Token![=],
-    pub ty: Type,
-}
-
-#[derive(Parse)]
-#[cfg_attr(test, derive(Debug, PartialEq, Eq))]
-pub struct TupleLike {
-    #[paren]
-    pub paren_token: token::Paren,
-    #[inside(paren_token)]
-    #[call(Punctuated::parse_terminated)]
-    pub fields: Punctuated<Type, Token![,]>,
-}
-
-#[derive(Parse)]
-#[cfg_attr(test, derive(Debug, PartialEq, Eq))]
-pub struct RecordLike {
-    #[brace]
-    pub brace_token: token::Brace,
-    #[inside(brace_token)]
-    #[call(Punctuated::parse_terminated)]
-    pub named_fields: Punctuated<NamedField, Token![,]>,
-}
-
-#[derive(Parse)]
-#[cfg_attr(test, derive(Debug, PartialEq, Eq))]
-pub struct NamedField {
-    pub ident: Ident,
-    pub colon_token: Token![:],
-    pub ty: Type,
+    pub colon: Option<Token![:]>,
+    #[parse_if(colon.is_some())]
+    pub ty: Option<Type>,
+    pub semi: Token![;],
 }
 
 #[derive(Parse)]
